7c8333864fefca7e6743934c22f9bf6fcc1ab91f
Rami3026 <91858420+Rami3026@users.noreply.github.com>
2021-11-08 10:07:40 +0000

    Add files via upload

---
+++ SecretMessage.js
@@ -0,0 +1,8 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push('wordOne', 'wordTwo');
+secretMessage['wordOne', 'wordTwo'] = 'WordThree';
+secretMessage.shift();
+secretMessage.unshift('WorldThree');
+console.log(secretMessage.join(' '));
